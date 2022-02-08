f0de244be137da937d9727caae30483348a6907d
Soushi Arakawa <soushi.arakawa@gmail.com>
2022-02-09 00:28:51 +0900

    create menu

--- sample_app/index.js
+++ sample_app/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, Menu, MenuItem, BrowserWindow } = require('electron')
 
 function createWindow() {
   let win = new BrowserWindow({
@@ -10,6 +10,34 @@ function createWindow() {
   })
   win.loadFile('index.html')
 }
+
+function createMenu() {
+  let menu = new Menu()
+
+  let file = new MenuItem({
+    label: 'File',
+    submenu:[
+      new MenuItem({ label: 'New' }),
+      new MenuItem({ label: 'File' }),
+      new MenuItem({ label: 'Quit'})
+    ]
+  })
+  menu.append(file)
+
+  let edit = new MenuItem({
+    label: 'Edit',
+    submenu: [
+      new MenuItem({ label: 'Cut' }),
+      new MenuItem({ label: 'Copy' }),
+      new MenuItem({ label: 'Paste' })
+    ]
+  })
+  menu.append(edit)
+
+  Menu.setApplicationMenu(menu)
+}
+
+createMenu()
 
 app.whenReady().then(createWindow)
 
