3984276d47a557e480601fcdb61ec31b77ebf144
Soushi Arakawa <soushi.arakawa@gmail.com>
2022-02-09 00:01:03 +0900

    set parent window

--- sample_app/index.js
+++ sample_app/index.js
@@ -10,12 +10,23 @@ const html = '<html><head>'
 function createWindow() {
   let win = new BrowserWindow({
     width: 400,
-    height: 200,
+    height: 300,
     webPreferences: {
       nodeINtegration: true
     }
   })
-  win.loadURL('data:text/html;charset=utf-8,' + html)
+  win.loadFile('index.html')
+
+  let child = new BrowserWindow({
+    width: 350,
+    height: 200,
+    parent: win,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  child.loadURL('data:text/html;charset=utf-8,' + html)
 }
 
 app.whenReady().then(createWindow)
