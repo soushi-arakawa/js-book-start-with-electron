6788935202c1a0ed461db56c3f839510bc50ceaf
Soushi Arakawa <soushi.arakawa@gmail.com>
2022-02-10 00:00:53 +0900

    get covid json

--- sample_app/preload.js
+++ sample_app/preload.js
@@ -4,6 +4,7 @@ contextBridge.exposeInMainWorld(
   'ipcMain', 
   {
     fs: () => require('fs'),
+    https: () => require('https'),
     showErrorMessage: (title, message) => ipcRenderer.invoke('showErrorMessage', title, message),
     showOpenDialog: () => ipcRenderer.invoke('showOpenDialog')
   }
