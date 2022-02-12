127a7a8217b202e73c81d8e1f734e674eb03c66e
Soushi Arakawa <soushi.arakawa@gmail.com>
2022-02-12 16:08:38 +0900

    apply theme

--- sample_editor/editor.js
+++ sample_editor/editor.js
@@ -3,6 +3,10 @@ let = editor = null
 window.addEventListener('DOMContentLoaded', onLoad)
 
 function onLoad() {
+  const editor_area = document.getElementById('editor_area')
+  const footer_area = document.getElementById('footer')
+
   editor = ace.edit('editor_area')
+  editor.setTheme('ace/theme/dracula')
   editor.focus()
 }
