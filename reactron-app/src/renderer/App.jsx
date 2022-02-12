05a99837cc249d10b258b3bb1919e43f0eee2c5d
Soushi Arakawa <soushi.arakawa@gmail.com>
2022-02-12 22:02:09 +0900

    change style on change selected index

--- reactron-app/src/renderer/App.jsx
+++ reactron-app/src/renderer/App.jsx
@@ -22,22 +22,35 @@ class App extends React.Component {
 class Content extends React.Component {
   constructor(props) {
     super(props)
-    this.fieldvalue = ''
+    this.style = [
+      {
+        color: 'red',
+        backgroundColor: '#ffdddd',
+        padding: '5px',
+        borderStyle: 'solid',
+        borderWidth: '5px',
+        borderColor: '#990000'
+      },
+      {
+        color: '#ddddff',
+        backgroundColor: 'blue',
+        padding: '5px',
+        borderStyle: 'double',
+        borderWidth: '7px',
+        borderColor: '#eeeeff'
+      }
+    ]
     this.state = {
-      data: []
+      style: this.style[0]
     }
     this.doChange = this.doChange.bind(this)
-    this.doAction = this.doAction.bind(this)
   }
 
   doChange(e) {
-    this.fieldvalue = e.target.value
-  }
-
-  doAction(e) {
-    this.state.data.push(this.fieldvalue)
+    console.log('doChange')
+    const n = e.target.selectedIndex
     this.setState(state => ({
-      data: state.data
+      style: this.style[n]
     }))
   }
 
@@ -45,18 +58,12 @@ class Content extends React.Component {
     return (
       <div className="container">
         <div className="alert alert-primary">
-          <h2>Content Component {this.state.count}</h2>
-          <p>This is Content-class component!!</p>
-          <ul className="list-group">
-            {this.state.data.map((val, index) => (
-              <li key={index} className="list-group-item">{val}</li>
-            ))}
-          </ul>
-          <hr />
-          <div className="row m-0">
-            <input type="text" className="form-control col-10" onChange={this.doChange} />
-            <button className="btn btn-primary col-2" onClick={this.doAction}>click</button>
-          </div>
+          <h2 style={this.state.style}>Content Component {this.state.count}</h2>
+          <p style={this.state.style}>This is Content-class component!!</p>
+          <select className="form-control" onChange={this.doChange}>
+            <option>red</option>
+            <option>blue</option>
+          </select>
         </div>
       </div>
     )
